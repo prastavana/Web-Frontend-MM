8f5fa30079eaea70cdfcb51f2eae6b8e2f13381c
prastavana <prastavanahere@gmail.com>
2024-12-23 18:38:54 +0545

    First commit frontend

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { lazy } from "react"
+import GettingStarted from "./core/public/gettingstarted.jsx";
+
+
+// import Login from "./core/public/login"
+const Login =lazy(()=>import("./core/public/login"))
+
+
+
+// import Layout from "./core/private/layout"
+const Layout =lazy(()=>import("./core/private/layout"))
+
+
+
+function App() {
+
+
+  const publicRoutes=[
+    // {path:"/",element:<Login/>},
+    {path:"/",element:<GettingStarted/>},
+    {path:"*",element:<>unauthorized</>}
+  ]
+
+  const privateRoutes=[
+    {path:"/admin",element:<Layout/>},
+    {path:"*",element:<>Page not found</>}
+  ]
+
+  // LOGIN logic TODO
+  const isAdmin=false;
+  const routes=isAdmin?privateRoutes:publicRoutes
+  return (
+      <>
+        <RouterProvider router={createBrowserRouter(routes)}/>
+      </>
+  )
+}
+
+export default App
