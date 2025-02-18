d0e3a3fd58c140ebf283469737017667d67a7085
prastavana <prastavanahere@gmail.com>
2025-02-18 19:50:58 +0545

    meter js

---
+++ src/components/meter.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+// eslint-disable-next-line react/prop-types
+const Meter = ({ cents }) => {
+    const rotation = Math.max(-50, Math.min(50, cents));
+
+    return (
+        <div style={styles.meter}>
+            <div style={{ ...styles.pointer, transform: `rotate(${rotation}deg)` }} />
+        </div>
+    );
+};
+
+const styles = {
+    meter: {
+        width: "200px",
+        height: "100px",
+        position: "relative",
+        borderBottom: "5px solid black",
+        margin: "20px auto",
+    },
+    pointer: {
+        width: "5px",
+        height: "50px",
+        backgroundColor: "red",
+        position: "absolute",
+        bottom: "0",
+        left: "50%",
+        transformOrigin: "bottom",
+    },
+};
+
+export default Meter;
